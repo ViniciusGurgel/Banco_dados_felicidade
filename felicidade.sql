3d73c1930ff9d6b829ae44955d31c83f39e6e9c0
Vinicius Gurgel Serrao <104094638+ViniciusGurgel@users.noreply.github.com>
2023-11-02 14:33:02 -0300

    Update felicidade.sql

--- felicidade.sql
+++ felicidade.sql
@@ -9,6 +9,7 @@ CREATE TABLE pessoa (
     nome VARCHAR(150) NOT NULL,
     data_nasc date NOT NULL,
     email VARCHAR(90) NOT NULL,
+    senha VARCHAR(90) NOT NULL,
     telefone varchar(11) NOT NULL,
     endereço varchar(150),
     CEP varchar(8) NOT NULL
@@ -20,43 +21,43 @@ CREATE TABLE setor (
 );
 
 CREATE TABLE tipo_ocorrencia (
-	id_tipo_ocorrencia INT AUTO_INCREMENT PRIMARY KEY,
-    id_setor INT, 
+    id_tipo_ocorrencia INT AUTO_INCREMENT PRIMARY KEY,
+    id_setor_fk INT, 
     tipo VARCHAR(30) NOT NULL UNIQUE,
-    FOREIGN KEY (id_setor) REFERENCES setor(id_setor)
+    FOREIGN KEY (id_setor_fk) REFERENCES setor(id_setor)
 );
 
 CREATE TABLE lugar (
-	id_lugar INT AUTO_INCREMENT PRIMARY KEY,
+    id_lugar INT AUTO_INCREMENT PRIMARY KEY,
     lugar_nome VARCHAR(90)
 );
 
 CREATE TABLE sub_lugar (
-	id_sub_lugar INT AUTO_INCREMENT PRIMARY KEY,
-    lugar_id INT,
+    id_sub_lugar INT AUTO_INCREMENT PRIMARY KEY,
+    id_lugar_fk INT,
     sub_local_name VARCHAR(90),
-    foreign key (lugar_id) REFERENCES lugar (id_lugar)
+    foreign key (id_lugar_fk) REFERENCES lugar (id_lugar)
 );
 
 CREATE TABLE ocorrencia (
     id_ocorrencia INT AUTO_INCREMENT PRIMARY KEY,
-    id_tipo_ocorrencia int,
-    id_pessoa_ref int,
-    id_sub_lugar int,
+    id_tipo_ocorrencia_fk int,
+    id_pessoa_fk int,
+    id_sub_lugar_fk int,
     descricao TEXT NOT NULL,
     data_ocorrencia DATE NOT NULL,
     localizacao VARCHAR(100) NOT NULL,
     data_registro datetime NOT NULL,
     ocorrencia_status BOOLEAN NOT NULL,
-    FOREIGN KEY (id_tipo_ocorrencia) REFERENCES tipo_ocorrencia (id_tipo_ocorrencia),
-    foreign key (id_sub_lugar) REFERENCES sub_lugar (id_sub_lugar),
-    FOREIGN KEY (id_pessoa_ref) REFERENCES pessoa(id_pessoa)
+    FOREIGN KEY (id_tipo_ocorrencia_fk) REFERENCES tipo_ocorrencia (id_tipo_ocorrencia),
+    foreign key (id_sub_lugar_fk) REFERENCES sub_lugar (id_sub_lugar),
+    FOREIGN KEY (id_pessoa_fk) REFERENCES pessoa(id_pessoa)
 );
 
 CREATE TABLE midia (
-	id_midia INT AUTO_INCREMENT PRIMARY KEY,
-    id_ocorrencia INT,
+    id_midia INT AUTO_INCREMENT PRIMARY KEY,
+    id_ocorrencia_fk INT,
     descrição text NOT NULL,
-	midia longblob,
-    FOREIGN KEY (id_ocorrencia) REFERENCES ocorrencia(id_ocorrencia)
+    midia longblob,
+    FOREIGN KEY (id_ocorrencia_fk) REFERENCES ocorrencia(id_ocorrencia)
 );
